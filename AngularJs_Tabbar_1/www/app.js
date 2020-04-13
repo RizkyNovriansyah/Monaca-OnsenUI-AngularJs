0f9590828f62556afec1bcda59e40f64e3e8f526
Rizky Novriansyah <rizkynovriansyah82.rn@gmail.com>
2020-04-13 21:20:55 +0700

    basic route

--- AngularJs_Tabbar_1/www/app.js
+++ AngularJs_Tabbar_1/www/app.js
@@ -1,8 +1,35 @@
 console.log("ha?")
-var module = angular.module('myApp', ['onsen']);
+var module = angular.module('myApp', ['onsen','ui.router']);
 
-module.controller('RootController', function($scope) {
+module.run(function ($location) {
+    angular.element(document).ready(function () {
+        
+    });
 });
+
+module.config([
+    "$stateProvider",
+    "$urlRouterProvider",
+    function (
+        $stateProvider,
+        $state,
+        $urlRouterProvider,
+        $location
+    ) {
+        // console.log("config", $location.path());
+        $stateProvider
+            .state("home", {
+                url: "/home",
+                templateUrl: "app/views/home.html",
+            })
+    }
+]);
+
+module.controller('RootController', function($scope,$state) {
+    console.log("config1");
+    $state.transitionTo("home");
+});
+
 module.controller('PageController', function($scope) {
     $scope.alert = function(message) {
     ons.notification.alert(message);
@@ -20,13 +47,16 @@ module.controller('KaryawansController', function($scope, KaryawansService) {
     console.log('KaryawansController');
     let ctrl = this;
     ctrl.karyawans = []
-    ctrl.refresh = function () {
+    ctrl.refresh = function ($done) {
         KaryawansService.get().then(function (response) {
             console.log('response', response.data.results);
             ctrl.karyawans = response.data.results;
+            $done();
         }, (errorRequest) => {
             console.log('errorRequest', errorRequest);
-            ons.notification.alert('error');
+            ons.notification.alert(errorRequest.data.detail);
+            ctrl.karyawans = [];
+            $done();
         });    
     }
     
@@ -34,8 +64,10 @@ module.controller('KaryawansController', function($scope, KaryawansService) {
 module.controller('LoginController', function($rootScope, LoginService) {
     console.log('LoginController');
     let ctrl = this;
-    this.token = $rootScope.token ? $rootScope : false;
-
+    ctrl.logout = function () {
+        TokenUser = ""; 
+        $rootScope.tokenExist = false;
+    }
     ctrl.login = function () {
         let username = ctrl.username;
         let password = ctrl.password;
@@ -48,8 +80,7 @@ module.controller('LoginController', function($rootScope, LoginService) {
             console.log('response', response);
             if (response.token) {
                 TokenUser = response.token; 
-                $rootScope.token = TokenUser;
-                this.token = $rootScope.token;
+                $rootScope.tokenExist = true;
                 ons.notification.alert('Logged in');
             }
 		}, (errorRequest) => {
